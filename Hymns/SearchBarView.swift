7a7717e3dd6c47e9d82edb9895545f1a224bc747
finben <benfindeisen@gmail.com>
2020-03-24 13:36:53 -0500

    Adding dummy and searchbar files

---
+++ Hymns/SearchBarView.swift
@@ -0,0 +1,42 @@
+//
+//  SearchBarView.swift
+//  Hymns
+//
+//  Created by Johanna Findeisen on 3/24/20.
+//  Copyright © 2020 skywalkerdude. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchBar: UIViewRepresentable {
+    
+    @Binding var text: String
+    
+    class Coordinator: NSObject, UISearchBarDelegate {
+        
+        @Binding var text: String
+        
+        init(text: Binding<String>) {
+            _text = text
+        }
+        
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+    
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        return searchBar
+    }
+    
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
