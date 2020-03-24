edd2bff4b98cc3b5d976e6f33c700f2af3418e9f
Luke Lu <skywalkerdude@gmail.com>
2020-03-23 22:17:15 -0700

    Initial Commit

---
+++ Hymns/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hymns
+//
+//  Created by Luke Lu on 3/23/20.
+//  Copyright © 2020 skywalkerdude. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HymnsTests/HymnsTests.swift
@@ -0,0 +1,34 @@
+//
+//  HymnsTests.swift
+//  HymnsTests
+//
+//  Created by Luke Lu on 3/23/20.
+//  Copyright © 2020 skywalkerdude. All rights reserved.
+//
+
+import XCTest
+@testable import Hymns
+
+class HymnsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
