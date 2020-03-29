18fb4613f0622f5b31acd456d6d10d4ddd8377ed
finben <benfindeisen@gmail.com>
2020-03-28 19:51:30 -0500

    Progress so far WIP

--- Hymns/DetailHymnScreen.swift
+++ Hymns/DetailHymnScreen.swift
@@ -1,54 +1,33 @@
-//
-//  DetailHymnScreen.swift
-//  Hymns
-//
-//  Created by Benjamin Findeisen on 3/28/20.
-//  Copyright © 2020 skywalkerdude. All rights reserved.
-//
-
 import SwiftUI
 
 struct DetailHymnScreen: View {
     var body: some View {
-            VStack {
-                GeometryReader { geometry in
-                    VStack {
-                        HStack {
-                            Image(systemName: "xmark")
-                            Spacer()
+        VStack {
+            GeometryReader { geometry in
+                VStack {
+                    HStack {
+                        Image(systemName: "xmark")
+                        Spacer()
                         Text("Hello")
-                            Spacer()
-                            Image(systemName: "heart")
-                        }                                                .frame(width: geometry.size.width/1.1)
+                        Spacer()
+                        Image(systemName: "heart")
+                    }.frame(width: geometry.size.width/1.1)
 
-                        
-                        
-                        HStack {
-                            Spacer()
-                            Text("Lyrics")
-                            Spacer()
-                            Text("Chords")
-                            Spacer()
-                            Text("Guitar")
-                            Spacer()
-                            Text("Piano")
-                            Spacer()
-                          }
-                        
-
-                        
-                        
+                    HStack {
+                        Spacer()
+                        Text("Lyrics")
+                        Spacer()
+                        Text("Chords")
+                        Spacer()
+                        Text("Guitar")
+                        Spacer()
+                        Text("Piano")
+                        Spacer()
                     }
-                        .aspectRatio(contentMode: .fit)
-                                    
-                        .background(Color.white.shadow(radius: 2)
-                    )
+                }.aspectRatio(contentMode: .fit).background(Color.white.shadow(radius: 2))
                     Spacer()
-                    
-                }
-
-                
-        }
+            }//end geometry
+        }//end outer VStack
     }
 }
 
