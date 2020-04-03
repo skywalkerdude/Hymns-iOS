bfa3537fe46467c76784890fe37af10c6f5c316d
Luke Lu <skywalkerdude@gmail.com>
2020-04-03 16:08:01 -0700

    Moved the home tab view into a new "Home" group

---
+++ Hymns/Home/HomeTab.swift
@@ -0,0 +1,38 @@
+import Foundation
+import SwiftUI
+
+enum HomeTab {
+    case home
+    case browse
+    case favorites
+    case settings
+}
+
+extension HomeTab {
+    
+    func getImage(_ isSelected: Bool) -> Image {
+        switch self {
+        case .home:
+            return Image(systemName: "magnifyingglass")
+        case .browse:
+            return isSelected ? Image(systemName: "book.fill") : Image(systemName: "book")
+        case .favorites:
+            return isSelected ? Image(systemName: "heart.fill") : Image(systemName: "heart")
+        case .settings:
+            return Image(systemName: "gear")
+        }
+    }
+    
+    var label: Text {
+        switch self {
+        case .home:
+            return Text("Search tab")
+        case .browse:
+            return Text("Favorites tab")
+        case .favorites:
+            return Text("Browse tab")
+        case .settings:
+            return Text("Settings tab")
+        }
+    }
+}
