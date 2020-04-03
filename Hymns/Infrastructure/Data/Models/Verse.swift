99e33f6332ee1452fe3696a9a75aa097aeff256a
finben <benfindeisen@gmail.com>
2020-04-03 18:42:51 -0500

    Just to get the stanza to fully display properly. Or at least closer to it.

--- Hymns/Infrastructure/Data/Models/Verse.swift
+++ Hymns/Infrastructure/Data/Models/Verse.swift
@@ -3,7 +3,7 @@ import Foundation
 /**
  * Structure of a Verse object.
  */
-struct Verse: Codable, Equatable {
+struct Verse: Codable, Equatable, Hashable {
     let verseType: VerseType
     let verseContent: [String]
     let transliteration: [String]?
