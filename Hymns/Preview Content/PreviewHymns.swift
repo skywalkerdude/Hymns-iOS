4bf96014a50d8094655ea0fdef83ba15b16e6bc7
Luke Lu <skywalkerdude@gmail.com>
2020-04-03 22:39:44 -0700

    Add some fake preview data so the previews will look nice during development

---
+++ Hymns/Preview Content/PreviewHymns.swift
@@ -0,0 +1,23 @@
+import Foundation
+import Resolver
+
+// swiftlint:disable all
+
+class PreviewHymns {
+    
+    static var decoder: JSONDecoder {
+        let decoder: JSONDecoder = Resolver.resolve()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+    
+    static var classic1151 = getHymnFromJson(fileName: "classic1151")
+    static var classic1334 = getHymnFromJson(fileName: "classic1334")
+    
+    static func getHymnFromJson(fileName: String) -> Hymn {
+        let jsonPath = Bundle.main.path(forResource: fileName, ofType: "json")!
+        let jsonString = try! String(contentsOfFile: jsonPath)
+        let jsonData = jsonString.data(using: .utf8)!
+        return try! decoder.decode(Hymn.self, from: jsonData)
+    }
+}
