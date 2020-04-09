e6cd7151007abd617bfbd5b1a7146684c4d18559
finben <benfindeisen@gmail.com>
2020-04-09 13:26:27 -0500

    Addressing most of the comments from PR

--- HymnsUITests/HymnsUITests.swift
+++ HymnsUITests/HymnsUITests.swift
@@ -24,7 +24,7 @@ class HymnsUITests: XCTestCase {
         //The setUp method is a good place to do this.
         
         // We send a command line argument to our app,
-             // to enable it to reset its state. Useful to remove flakiness
+        // to enable it to reset its state. Useful to remove flakiness
         app.launchArguments.append("--uitesting")
     }
 
@@ -32,7 +32,7 @@ class HymnsUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func test_all_tab_bars() {
+    func test_navigateBetweenTabs() {
         app.launch()
         let tabBarsQuery = app.tabBars
         let tab1 = tabBarsQuery.children(matching: .button).element(boundBy: 0) //Home
@@ -40,15 +40,15 @@ class HymnsUITests: XCTestCase {
         let tab3 = tabBarsQuery.children(matching: .button).element(boundBy: 2) //Favorites
         let tab4 = tabBarsQuery.children(matching: .button).element(boundBy: 3) //Settings
 
+        //TODO: Assertions after each tap
         tab1.tap()
         tab4.tap() //Settings
         tab3.tap()
         tab2.tap()
         tab1.tap()
         tab2.tap()
-                
     }
-    
+
     func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
