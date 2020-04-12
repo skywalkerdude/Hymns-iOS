c14ee3ff00395be71b11e40443b5da0bbc3b9a33
Luke Lu <skywalkerdude@gmail.com>
2020-04-12 00:40:59 -0700

    Migrate two more tests to use Quick/Nimble. Though, using Quick with Combine is a little funky, since we no longer have 1 assertion per "it" clause. We have to assert both the expectation and value in the same it clause. I suppose it's not worse than the old test, but I don't know if it's THAT much better. The test case names a much longer and the files are way more indented. It also doesn't allow us to avoid using XCTestExpectations, which was promised by Nimble. I filed an issue here: https://github.com/Quick/Quick/issues/963. Let's see what they say.

--- Hymns/Infrastructure/Data/ErrorType.swift
+++ Hymns/Infrastructure/Data/ErrorType.swift
@@ -11,9 +11,9 @@ extension ErrorType: LocalizedError {
     public var errorDescription: String? {
         switch self {
         case .parsing:
-            return NSLocalizedString("Error occured when parsing a network response", comment: "Error occured when parsing a network response")
+            return NSLocalizedString("Error occurred when parsing a network response", comment: "Error occurred when parsing a network response")
         case .network:
-            return NSLocalizedString("Error ocurred when making a network request", comment: "Error ocurred when making a network request")
+            return NSLocalizedString("Error occurred when making a network request", comment: "Error ocurred when making a network request")
         }
     }
 }
