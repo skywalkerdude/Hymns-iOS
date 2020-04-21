6b01ba1fd3949ac52f9a6490732dac10c5d07910
finben <benfindeisen@gmail.com>
2020-04-21 13:51:51 -0500

    test

--- Hymns/Display/DisplayHymnViewModel.swift
+++ Hymns/Display/DisplayHymnViewModel.swift
@@ -1,7 +1,7 @@
 import Combine
 import Resolver
 import SwiftUI
-
+  
 //This view model will expose 4 other view models to be passed through detailhymnscreen to their respective views. HymnLyricsViewModel, GuitarViewModel, PianoViewModel, and ChordsView Model
 //ex. HymnLyricsViewModel -> DetailHymnScreenViewModel -> DetailHymnScreen -> HymnLyricsView
 class DisplayHymnViewModel: ObservableObject {
