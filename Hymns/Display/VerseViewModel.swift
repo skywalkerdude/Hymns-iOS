7bc48460c55ba6fd7fa81d220bf32b7c0daff550
Samuel Yusuf <styusuf11@gmail.com>
2020-04-22 22:08:51 -0400

    made verse a let property instead of a var in VerseViewModel

--- Hymns/Display/VerseViewModel.swift
+++ Hymns/Display/VerseViewModel.swift
@@ -1,17 +1,10 @@
-//
-//  VerseViewModel.swift
-//  Hymns
-//
-//  Created by Samuel Yusuf on 4/18/20.
-//  Copyright © 2020 skywalkerdude. All rights reserved.
-//
-
 import Foundation
 
 struct VerseViewModel: Hashable {
 
     var verseNumber: String?
-    var verse: Verse
+    let verse: Verse
+    
     init(verseNumber: String, verse: Verse) {
         self.verseNumber = verseNumber
         self.verse = verse
