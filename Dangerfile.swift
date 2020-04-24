755971e0be36882db1819cf459575615d9f37e33
Luke Lu <skywalkerdude@gmail.com>
2020-04-24 01:09:18 -0700

    coverage

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,10 +1,9 @@
 // swiftlint:disable all
 import Danger
-import DangerXCodeSummary // package: [https://github.com/f-meloni/danger-swift-xcodesummary.git]
+import DangerSwiftCoverage // package: https://github.com/f-meloni/danger-swift-coverage.git
 
 let danger = Danger()
 
 let files = danger.git.modifiedFiles.filter { $0.hasPrefix("Hymns") }
 SwiftLint.lint(.files(files), inline: true, configFile: ".swiftlint.yml")
 
-//let summary = XCodeSummary(filePath: "result.json")
