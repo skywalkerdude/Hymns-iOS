a3d3ca14bf9306eb14c481649bc9013506ca9516
Luke Lu <skywalkerdude@gmail.com>
2020-04-24 00:45:24 -0700

    should pass

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,20 +1,13 @@
 // swiftlint:disable all
 import Danger
-import DangerXCodeSummary // package: https://github.com/f-meloni/danger-swift-xcodesummary.git
 
 let danger = Danger()
-
-let files = danger.git.modifiedFiles.filter { $0.hasPrefix("Hymns") }
-SwiftLint.lint(.files(files), inline: true, configFile: ".swiftlint.yml")
-
-//let summary = XCodeSummary(filePath: "result.json")
 
 // Ensure no copyright header
 let changedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 let swiftFilesWithCopyright = changedFiles.filter {
     $0.fileType == .swift && danger.utils.readFile($0).contains("//  Created by")
 }
-
 if swiftFilesWithCopyright.count > 0 {
     let files = swiftFilesWithCopyright.joined(separator: ", ")
     fail("Please remove the copyright header in: \(files)")
