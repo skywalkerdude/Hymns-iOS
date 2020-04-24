6b3ddc48dc6fe57e672ebdb5147b9b883316297d
Luke Lu <skywalkerdude@gmail.com>
2020-04-24 00:10:31 -0700

    add a plugin?

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,11 +1,13 @@
 // swiftlint:disable all
 import Danger
+import DangerXCodeSummary // package: https://github.com/f-meloni/danger-swift-xcodesummary.git
 
 let danger = Danger()
 
 let files = danger.git.modifiedFiles.filter { $0.hasPrefix("Hymns") }
-
 SwiftLint.lint(.files(files), inline: true, configFile: ".swiftlint.yml")
+
+let summary = XCodeSummary(filePath: "result.json")
 
 // Ensure no copyright header
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
