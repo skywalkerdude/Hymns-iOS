ec31aba32794c1603d4712fd544ea525e1aa2d56
Luke Lu <skywalkerdude@gmail.com>
2020-04-23 23:41:02 -0700

    testing some danger stuff

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -11,12 +11,12 @@ let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 let swiftFilesWithCopyright = editedFiles.filter {
     $0.contains("Copyright") && ($0.fileType == .swift  || $0.fileType == .m)
 }
-for file in swiftFilesWithCopyright {
+for file in editedFiles {
     fail(message: "Please remove this copyright header", file: file, line: 0)
 }
 
 // Encourage smaller PRs
-var bigPRThreshold = 600;
+var bigPRThreshold = 1000;
 if (danger.github.pullRequest.additions! + danger.github.pullRequest.deletions! > bigPRThreshold) {
     warn("> Pull Request size seems relatively large. If this Pull Request contains multiple changes, please split each into separate PR will helps faster, easier review.");
 }
