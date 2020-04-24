1e1482aab59b3e3048e67612d3a36eda66bf695e
Luke Lu <skywalkerdude@gmail.com>
2020-04-24 10:18:57 -0700

    try to get coverage going

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,5 +1,6 @@
 // swiftlint:disable all
 import Danger
+import DangerSwiftCoverage // package: https://github.com/f-meloni/danger-swift-coverage.git
 
 let danger = Danger()
 
@@ -20,3 +21,5 @@ var bigPRThreshold = 1000;
 if (danger.github.pullRequest.additions! + danger.github.pullRequest.deletions! > bigPRThreshold) {
     warn("> Pull Request size seems relatively large. If this Pull Request contains multiple changes, please split each into separate PR will helps faster, easier review.");
 }
+
+Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), minimumCoverage: 50, excludedTargets: ["DangerSwiftCoverageTests.xctest"])
