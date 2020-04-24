9fd7186d33c9bf8bd10f140b0c4e8fb8e4fe6dfe
Luke Lu <skywalkerdude@gmail.com>
2020-04-24 01:03:16 -0700

    hmm

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,6 +1,6 @@
 // swiftlint:disable all
 import Danger
-//import DangerXCodeSummary // package: https://github.com/f-meloni/danger-swift-xcodesummary.git
+import DangerXCodeSummary // package: [https://github.com/f-meloni/danger-swift-xcodesummary.git]
 
 let danger = Danger()
 
@@ -8,20 +8,3 @@ let files = danger.git.modifiedFiles.filter { $0.hasPrefix("Hymns") }
 SwiftLint.lint(.files(files), inline: true, configFile: ".swiftlint.yml")
 
 //let summary = XCodeSummary(filePath: "result.json")
-
-// Ensure no copyright header
-let changedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-let swiftFilesWithCopyright = changedFiles.filter {
-    $0.fileType == .swift && danger.utils.readFile($0).contains("//  Created by")
-}
-
-if swiftFilesWithCopyright.count > 0 {
-    let files = swiftFilesWithCopyright.joined(separator: ", ")
-    fail("Please remove the copyright header in: \(files)")
-}
-
-// Encourage smaller PRs
-var bigPRThreshold = 1000;
-if (danger.github.pullRequest.additions! + danger.github.pullRequest.deletions! > bigPRThreshold) {
-    warn("> Pull Request size seems relatively large. If this Pull Request contains multiple changes, please split each into separate PR will helps faster, easier review.");
-}
