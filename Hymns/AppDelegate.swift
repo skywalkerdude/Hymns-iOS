fec50db99b60db139bdb44de0c787b23c03be46a
finben <benfindeisen@gmail.com>
2020-04-26 17:45:30 -0500

    Moving away from Fabric to Beta FirebaseCrashlytics SDK

--- Hymns/AppDelegate.swift
+++ Hymns/AppDelegate.swift
@@ -1,7 +1,7 @@
-import Fabric
 import Firebase
 import CoreData
 import UIKit
+import FirebaseCrashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -9,7 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        Fabric.sharedSDK().debug = true
+        Crashlytics.crashlytics()
         return true
     }
 
