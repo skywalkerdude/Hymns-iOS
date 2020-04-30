da08f4ae8d10ca75110dd4cdc02aa8ce920b6aca
Luke Lu <skywalkerdude@gmail.com>
2020-04-29 22:41:16 -0700

    ehh

--- Hymns/Common/PagerView.swift
+++ Hymns/Common/PagerView.swift
@@ -10,23 +10,25 @@ struct PagerView<Content: View>: View {
 
     let pageCount: Int
     @Binding var currentIndex: Int
-    let content: Content
+    let viewBuilder: (_ page: Int) -> Content
 
     @GestureState private var translation: CGFloat = 0
 
-    init(pageCount: Int, currentIndex: Binding<Int>, @ViewBuilder content: () -> Content) {
+    init(pageCount: Int, currentIndex: Binding<Int>, @ViewBuilder content: @escaping (_ page: Int) -> Content) {
         self.pageCount = pageCount
         self._currentIndex = currentIndex
-        self.content = content()
+        self.viewBuilder = content
     }
 
     var body: some View {
         GeometryReader { geometry in
             HStack(spacing: 0) {
-                self.content.frame(width: geometry.size.width)
+                self.doIt(geometry).frame(width: geometry.size.width)
             }
             .frame(width: geometry.size.width, alignment: .leading)
-            .offset(x: -CGFloat(self.currentIndex) * geometry.size.width)
+//            .offset(x: -CGFloat(self.currentIndex) * geometry.size.width)
+                // TODO this thing gotta be not 0 all the time
+                .offset(x: -CGFloat(self.figureOut()) * geometry.size.width)
             .offset(x: self.translation)
             .animation(.interactiveSpring())
             .gesture(
@@ -34,29 +36,70 @@ struct PagerView<Content: View>: View {
                     state = value.translation.width
                 }.onEnded { value in
                     let offset = value.translation.width / geometry.size.width
+                    print("offset: \(offset)")
                     let thresholdHit = abs(offset) > self.pageChangeOffset
                     if !thresholdHit {
                         return
                     }
                     let newIndex = self.currentIndex + (offset > 0 ? -1 : 1)
+                    print("newIndex: \(newIndex)")
                     self.currentIndex = min(max(Int(newIndex), 0), self.pageCount - 1)
+                    print("self.currentIndex = min(max(Int(\(newIndex)), 0), \(self.pageCount) - 1) = \(self.currentIndex)")
                 }
             )
         }
+    }
+
+    func doIt(_ geometry: GeometryProxy) -> ForEach<Range<Int>, Int, Content> {
+        var items = [Int]()
+        if self.currentIndex != 0 {
+            items.append(-1)
+        }
+        items.append(0)
+        if self.currentIndex < self.pageCount {
+            items.append(1)
+        }
+        print("currentIndex: \(currentIndex) items: \([items])")
+        return ForEach(items.indices, id: \.self) { idx in
+            self.dodo(items[idx])
+        }
+    }
+
+    func dodo(_ idx: Int) -> Content {
+        print("currentIndex: \(self.currentIndex) idx: \(idx)")
+        return self.viewBuilder(self.currentIndex + idx)
+    }
+
+    func figureOut() -> Int {
+        if self.currentIndex == 0 {
+            return 0
+        }
+        return 1
     }
 }
 
 struct PagerView_Previews: PreviewProvider {
     static var previews: some View {
-        var page = 0
+        var page = 3
         let currentPageBinding = Binding<Int>(
             get: {page},
             set: {page = $0})
-        return PagerView(pageCount: 300, currentIndex: currentPageBinding) {
-            ForEach(1..<100) { _ in
-                Color.blue
-                Color.red
-                Color.green
+
+        var colors = [Color]()
+        for num in 1...50 {
+            colors.append(.blue)
+            colors.append(.green)
+            colors.append(.red)
+        }
+
+        var nums = [Int]()
+        for num in 1...300 {
+            nums.append(num)
+        }
+        return PagerView(pageCount: 50, currentIndex: currentPageBinding) { page in
+            VStack {
+                Text("page \(nums[page])")
+                colors[page]
             }
         }
     }
--- Hymns/Display/DisplayHymnContainerView.swift
+++ Hymns/Display/DisplayHymnContainerView.swift
@@ -9,16 +9,23 @@ struct DisplayHymnContainerView: View {
     }
 
     var body: some View {
-        PagerView(pageCount: viewModel.hymns.count, currentIndex: $viewModel.currentIndex) {
-            ForEach(viewModel.hymns) { hymn in
-                if hymn.isLoaded {
-                    DisplayHymnView(viewModel: hymn)
-                } else {
-                    // TODO replace with View Extension in https://github.com/HymnalDreamTeam/Hymns-iOS/pull/99
-                    ActivityIndicator().frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 0, idealHeight: .infinity, maxHeight: .infinity, alignment: .center)
-                }
-            }
+        PagerView(pageCount: viewModel.hymns.count, currentIndex: $viewModel.currentIndex) { page in
+            self.bbb(page)
         }
+//            ForEach(viewModel.hymns) { hymn in
+//                if hymn.isLoaded {
+//                    DisplayHymnView(viewModel: hymn)
+//                } else {
+//                    // TODO replace with View Extension in https://github.com/HymnalDreamTeam/Hymns-iOS/pull/99
+//                    ActivityIndicator().frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 0, idealHeight: .infinity, maxHeight: .infinity, alignment: .center)
+//                }
+//            }
+//        }
+    }
+
+    func bbb(_ page: Int) -> DisplayHymnView {
+        print("page \(page): \(self.viewModel.hymns[page])")
+        return DisplayHymnView(viewModel: self.viewModel.hymns[page])
     }
 }
 
