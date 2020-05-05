9f4130a646d17f22b1cd28d921ec1a064f089512
finben <benfindeisen@gmail.com>
2020-05-05 14:18:18 -0500

    Large tab bar icons, weight fix, and padding fix (#113)
    * Increases the size of our tab bar nav icons as Emily requested
    
    * Making weight heavier and making padding consistent with defaults

--- Hymns/Common/FontViewModifier.swift
+++ Hymns/Common/FontViewModifier.swift
@@ -2,10 +2,10 @@ import SwiftUI
 
 //The custom title view modifier is meant to be set up to replace navigationbartitle texts. By doing this we gain alot more ability to customize the title text at the top of the screen. In addition, we also avoid being forced into using navigation bar titles  which comes with other issues as well.
 struct CustomTitleLayout: ViewModifier {
-    let font = Font.title.weight(.semibold)
+    let font = Font.title.weight(.bold)
     func body(content: Content) -> some View {
         content
-            .padding(EdgeInsets(top: 20, leading: 10, bottom: 10, trailing: 0))
+            .padding()
             .font(font)
     }
 }
