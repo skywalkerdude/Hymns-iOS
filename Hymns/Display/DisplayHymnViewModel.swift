d9b528f6f4c49eab89ddff9c21cb72adec6df717
Luke Lu <skywalkerdude@gmail.com>
2020-05-08 14:56:10 -0700

    remove super annoying trailing whitespace... need to figure out how to automate getting rid of that -__-

--- Hymns/Display/DisplayHymnViewModel.swift
+++ Hymns/Display/DisplayHymnViewModel.swift
@@ -5,10 +5,10 @@ import SwiftUI
 
 class DisplayHymnViewModel: ObservableObject {
     let webviewCache = WebViewPreloader()
-    
+
     typealias Title = String
     typealias Lyrics = [Verse]
-    
+
     @Published var title: String = ""
     var hymnLyricsViewModel: HymnLyricsViewModel
     private let backgroundQueue: DispatchQueue
@@ -22,10 +22,10 @@ class DisplayHymnViewModel: ObservableObject {
     @Published var guitarUrl: URL?
     @Published var pianoUrl: URL?
     @Published var isFavorited: Bool?
-    
+
     private var favoritesObserver: Notification?
     private var disposables = Set<AnyCancellable>()
-    
+
     init(backgroundQueue: DispatchQueue = Resolver.resolve(name: "background"),
          hymnToDisplay identifier: HymnIdentifier,
          hymnsRepository repository: HymnsRepository = Resolver.resolve(),
@@ -40,11 +40,11 @@ class DisplayHymnViewModel: ObservableObject {
         self.historyStore = historyStore
         hymnLyricsViewModel = HymnLyricsViewModel(hymnToDisplay: identifier)
     }
-    
+
     deinit {
         favoritesObserver = nil
     }
-    
+
     func fetchHymn() {
         repository
             .getHymn(identifier)
@@ -54,7 +54,7 @@ class DisplayHymnViewModel: ObservableObject {
                 receiveValue: { [weak self] hymn in
                     guard let self = self else { return }
                     guard let hymn = hymn else { return }
-                    
+
                     let title: Title
                     if self.identifier.hymnType == .classic {
                         title = "Hymn \(self.identifier.hymnNumber)"
@@ -62,7 +62,7 @@ class DisplayHymnViewModel: ObservableObject {
                         title = hymn.title.replacingOccurrences(of: "Hymn: ", with: "")
                     }
                     self.title = title
-                    
+
                     self.lyrics = hymn.lyrics
                     let chordsPath = hymn.pdfSheet?.data.first(where: { datum -> Bool in
                         datum.value == DatumValue.text.rawValue
@@ -73,7 +73,7 @@ class DisplayHymnViewModel: ObservableObject {
                     if let chordsUrl = self.chordsUrl {
                         self.webviewCache.preload(url: chordsUrl)
                     }
-                    
+
                     let guitarPath = hymn.pdfSheet?.data.first(where: { datum -> Bool in
                         datum.value == DatumValue.guitar.rawValue
                     })?.path
@@ -83,7 +83,7 @@ class DisplayHymnViewModel: ObservableObject {
                     if let guitarUrl = self.guitarUrl {
                         self.webviewCache.preload(url: guitarUrl)
                     }
-                    
+
                     let pianoPath = hymn.pdfSheet?.data.first(where: { datum -> Bool in
                         datum.value == DatumValue.piano.rawValue
                     })?.path
@@ -93,12 +93,12 @@ class DisplayHymnViewModel: ObservableObject {
                     if let pianoUrl = self.pianoUrl {
                         self.webviewCache.preload(url: pianoUrl)
                     }
-                    
+
                     self.fetchFavoriteStatus()
                     self.historyStore.storeRecentSong(hymnToStore: self.identifier, songTitle: title)
             }).store(in: &disposables)
     }
-    
+
     func fetchFavoriteStatus() {
         self.isFavorited = favoritesStore.isFavorite(hymnIdentifier: identifier)
         favoritesObserver = favoritesStore.observeFavoriteStatus(hymnIdentifier: identifier) { isFavorited in
