ca0a653ed874ca600fbbb4b36aa375745f836661
Luke Lu <skywalkerdude@gmail.com>
2020-05-09 22:36:37 -0700

    Fix the database crash by copying the database file over to the Application Support directly. Also created a bunch of fallbacks in case the database creation/copying failed.
    1) Try to import bundled database (hymnaldb-v12.sqlite). If that fails...
    2) Create a new database file and initialize it with empty tables. If that fails...
    3) Create an in-memory database and initialize it with empty tables. And if all fails...
    4) Indicate that the database isn't initialized correctly so that other classes will know to not use it
    
    Ideally, I would also like Firebase Analytics reporting to know how often each of these steps fails. Created a TODO for that.

---
+++ HymnsTests/Repository/HymnsRepositoryImpl_dbUninitializedTests.swift
@@ -0,0 +1,133 @@
+import Combine
+import Mockingbird
+import XCTest
+@testable import Hymns
+
+class HymnsRepositoryImpl_dbUninitializedTests: XCTestCase {
+
+    let databaseResult = HymnEntity(hymnIdentifier: cebuano123,
+                                    id: 0,
+                                    title: "song title",
+                                    lyricsJson: "[{\"verse_type\":\"verse\",\"verse_content\":[\"line 1\",\"line 2\"]}]")
+    let networkResult = Hymn(title: "song title", metaData: [MetaDatum](), lyrics: [Verse(verseType: .verse, verseContent: ["line 1", "line 2"])])
+    let expected = UiHymn(hymnIdentifier: cebuano123, title: "song title", lyrics: [Verse(verseType: .verse, verseContent: ["line 1", "line 2"])])
+
+    var backgroundQueue = DispatchQueue.init(label: "background test queue")
+    var converter: ConverterMock!
+    var dataStore: HymnDataStoreMock!
+    var service: HymnalApiServiceMock!
+    var systemUtil: SystemUtilMock!
+    var target: HymnsRepository!
+
+    override func setUp() {
+        super.setUp()
+        converter = mock(Converter.self)
+        dataStore = mock(HymnDataStore.self)
+        service = mock(HymnalApiService.self)
+        systemUtil = mock(SystemUtil.self)
+        target = HymnsRepositoryImpl(converter: converter, dataStore: dataStore, mainQueue: backgroundQueue, service: service, systemUtil: systemUtil)
+        given(dataStore.getDatabaseInitializedProperly()) ~> false
+    }
+
+    func test_getHymn_databaseError_noNetwork() {
+        given(dataStore.getHymn(cebuano123)) ~> { _ in
+            Just(nil).mapError({ (_) -> ErrorType in
+                .data(description: "This will never get called")
+            }).eraseToAnyPublisher()
+        }
+        given(systemUtil.isNetworkAvailable()) ~> false
+        given(converter.toUiHymn(hymnIdentifier: cebuano123, hymnEntity: nil)) ~> nil
+
+        let valueReceived = expectation(description: "value received")
+        let cancellable = target.getHymn(cebuano123)
+            .print(self.description)
+            .sink(receiveValue: { hymn in
+                valueReceived.fulfill()
+                XCTAssertNil(hymn)
+            })
+
+        verify(dataStore.getDatabaseInitializedProperly()).wasCalled(exactly(1))
+        verify(service.getHymn(any())).wasNeverCalled()
+        verify(dataStore.saveHymn(any())).wasNeverCalled()
+        wait(for: [valueReceived], timeout: testTimeout)
+        cancellable.cancel()
+    }
+
+    func test_getHymn_databaseError_networkAvailable_networkError() {
+        given(systemUtil.isNetworkAvailable()) ~> true
+        given(service.getHymn(cebuano123)) ~> { _ in
+            Just(self.networkResult)
+                .tryMap({ (_) -> Hymn in
+                    throw URLError(.badServerResponse)
+                })
+                .mapError({ (_) -> ErrorType in
+                    ErrorType.data(description: "forced network error")
+                }).eraseToAnyPublisher()
+        }
+
+        let valueReceived = expectation(description: "value received")
+        let cancellable = target.getHymn(cebuano123)
+            .print(self.description)
+            .sink(receiveValue: { hymn in
+                valueReceived.fulfill()
+                XCTAssertNil(hymn)
+            })
+
+        verify(dataStore.getDatabaseInitializedProperly()).wasCalled(exactly(1))
+        verify(service.getHymn(cebuano123)).wasCalled(exactly(1))
+        verify(dataStore.saveHymn(any())).wasNeverCalled()
+        wait(for: [valueReceived], timeout: testTimeout)
+        cancellable.cancel()
+    }
+
+    func test_getHymn_databaseError_networkAvailable_resultsSuccessful() {
+        given(systemUtil.isNetworkAvailable()) ~> true
+        given(service.getHymn(cebuano123)) ~> {  _ in
+            return Just(self.networkResult).mapError({ (_) -> ErrorType in
+                .data(description: "This will never get called")
+            }).eraseToAnyPublisher()
+        }
+        given(converter.toHymnEntity(hymnIdentifier: cebuano123, hymn: self.networkResult)) ~> self.databaseResult
+        given(converter.toUiHymn(hymnIdentifier: cebuano123, hymnEntity: self.databaseResult)) ~> self.expected
+
+        let valueReceived = expectation(description: "value received")
+        let cancellable = target.getHymn(cebuano123)
+            .print(self.description)
+            .sink(receiveValue: { hymn in
+                valueReceived.fulfill()
+                XCTAssertEqual(self.expected, hymn!)
+            })
+
+        verify(dataStore.getDatabaseInitializedProperly()).wasCalled(exactly(1))
+        verify(service.getHymn(cebuano123)).wasCalled(exactly(1))
+        verify(dataStore.saveHymn(self.databaseResult)).wasCalled(exactly(1))
+        wait(for: [valueReceived], timeout: testTimeout)
+        cancellable.cancel()
+    }
+
+    func test_getHymn_databaseError_networkConversionError() {
+        given(systemUtil.isNetworkAvailable()) ~> true
+        given(service.getHymn(cebuano123)) ~> {  _ in
+            Just(self.networkResult).mapError({ (_) -> ErrorType in
+                .data(description: "This will never get called")
+            }).eraseToAnyPublisher()
+        }
+        given(converter.toHymnEntity(hymnIdentifier: cebuano123, hymn: self.networkResult)) ~> {_, _ in
+            throw TypeConversionError.init(triggeringError: ErrorType.parsing(description: "failed to convert!"))
+        }
+
+        let valueReceived = expectation(description: "value received")
+        let cancellable = target.getHymn(cebuano123)
+            .print(self.description)
+            .sink(receiveValue: { hymn in
+                valueReceived.fulfill()
+                XCTAssertNil(hymn)
+            })
+
+        verify(dataStore.getDatabaseInitializedProperly()).wasCalled(exactly(1))
+        verify(service.getHymn(cebuano123)).wasCalled(exactly(1))
+        verify(dataStore.saveHymn(any())).wasNeverCalled()
+        wait(for: [valueReceived], timeout: testTimeout)
+        cancellable.cancel()
+    }
+}
--- HymnsTests/Repository/Infrastructure/Data/HymnDataStoreGrdbImplSpec.swift
+++ HymnsTests/Repository/Infrastructure/Data/HymnDataStoreGrdbImplSpec.swift
@@ -12,55 +12,7 @@ class HymnDataStoreGrdbImplSpec: QuickSpec {
             beforeEach {
                 // https://github.com/groue/GRDB.swift/blob/master/README.md#database-queues
                 inMemoryDBQueue = DatabaseQueue()
-                // CREATE TABLE SONG_DATA(
-                //   ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                //   HYMN_TYPE TEXT NOT NULL,
-                //   HYMN_NUMBER TEXT NOT NULL,
-                //   QUERY_PARAMS TEXT NOT NULL,
-                //   SONG_TITLE TEXT,
-                //   SONG_LYRICS TEXT,
-                //   SONG_META_DATA_CATEGORY TEXT,
-                //   SONG_META_DATA_SUBCATEGORY TEXT,
-                //   SONG_META_DATA_AUTHOR TEXT,
-                //   SONG_META_DATA_COMPOSER TEXT,
-                //   SONG_META_DATA_KEY TEXT,
-                //   SONG_META_DATA_TIME TEXT,
-                //   SONG_META_DATA_METER TEXT,
-                //   SONG_META_DATA_SCRIPTURES TEXT,
-                //   SONG_META_DATA_HYMN_CODE TEXT,
-                //   SONG_META_DATA_MUSIC TEXT,
-                //   SONG_META_DATA_SVG_SHEET_MUSIC TEXT,
-                //   SONG_META_DATA_PDF_SHEET_MUSIC TEXT,
-                //   SONG_META_DATA_LANGUAGES TEXT,
-                //   SONG_META_DATA_RELEVANT TEXT
-                // )
-                inMemoryDBQueue.inDatabase { database in
-                    // Don't worry about force_try in tests.
-                    // swiftlint:disable:next force_try
-                    try! database.create(table: "SONG_DATA") { table in
-                        table.autoIncrementedPrimaryKey(HymnEntity.CodingKeys.id.rawValue)
-                        table.column(HymnEntity.CodingKeys.hymnType.rawValue, .text).notNull()
-                        table.column(HymnEntity.CodingKeys.hymnNumber.rawValue, .text).notNull()
-                        table.column(HymnEntity.CodingKeys.queryParams.rawValue, .text).notNull()
-                        table.column(HymnEntity.CodingKeys.title.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.lyricsJson.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.category.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.subcategory.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.author.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.composer.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.key.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.time.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.meter.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.scriptures.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.hymnCode.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.musicJson.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.svgSheetJson.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.pdfSheetJson.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.languagesJson.rawValue, .text)
-                        table.column(HymnEntity.CodingKeys.relevantJson.rawValue, .text)
-                    }
-                }
-                target = HymnDataStoreGrdbImpl(databaseQueue: inMemoryDBQueue)
+                target = HymnDataStoreGrdbImpl(databaseQueue: inMemoryDBQueue, initializeTables: true)
             }
 
             describe("save a few songs") {
