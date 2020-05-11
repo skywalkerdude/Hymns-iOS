080eb5afdb496354269c1634e6da2d24be778022
finben <benfindeisen@gmail.com>
2020-05-11 17:28:27 -0500

    Closer to working. Only the first webview populates though.. idk why?

--- Hymns/Common/TabView/TabView.swift
+++ Hymns/Common/TabView/TabView.swift
@@ -6,15 +6,16 @@ import SwiftUI
  * Idea for this `TabView` class taken from: https://github.com/innoreq/IRScrollableTabView
  */
 public struct IndicatorTabView<TabType: TabItem>: View {
-
+    
     @Binding var currentTab: TabType
     let tabItems: [TabType]
-
-
+    @ObservedObject var viewModel: DisplayHymnViewModel
+    
+    
     public var body: some View {
         return
             VStack(alignment: .center) {
-
+                
                 Rectangle()
                     .shadow(radius: 0, y: -0.2)
                     .frame(height: 50)
@@ -23,36 +24,37 @@ public struct IndicatorTabView<TabType: TabItem>: View {
                 
                 Rectangle()
                     .foregroundColor(.white)
-                    .overlay(currentTab.content)
+                    .overlay( HymnSelectedTagView(currentTab: $currentTab, viewModel: self.viewModel)
+                )
         }
     }
 }
-
-struct IndicatorTabView_Previews: PreviewProvider {
-
-    static var previews: some View {
-        var selectedTab: HomeTab = .home
-        let selectedTabBinding = Binding<HomeTab>(
-            get: {selectedTab},
-            set: {selectedTab = $0})
-        let tabItems: [HomeTab] = [
-            .home,
-            .browse,
-            .favorites,
-            .settings
-        ]
-
-        return Group {
-            IndicatorTabView(currentTab: selectedTabBinding, tabItems: tabItems)
-                .previewDevice(PreviewDevice(rawValue: "iPhone XS Max"))
-                .previewDisplayName("iPhone XS Max")
-            IndicatorTabView(currentTab: selectedTabBinding, tabItems: tabItems)
-                .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
-                .previewDisplayName("iPhone SE")
-            IndicatorTabView(currentTab: selectedTabBinding, tabItems: tabItems)
-                .previewDevice(PreviewDevice(rawValue: "iPad Air 2"))
-                .previewDisplayName("iPad Air 2")
-        }
-    }
-}
-
+//
+//struct IndicatorTabView_Previews: PreviewProvider {
+//
+//    static var previews: some View {
+//        var selectedTab: HomeTab = .home
+//        let selectedTabBinding = Binding<HomeTab>(
+//            get: {selectedTab},
+//            set: {selectedTab = $0})
+//        let tabItems: [HomeTab] = [
+//            .home,
+//            .browse,
+//            .favorites,
+//            .settings
+//        ]
+//
+//        return Group {
+//            IndicatorTabView(currentTab: selectedTabBinding, tabItems: tabItems)
+//                .previewDevice(PreviewDevice(rawValue: "iPhone XS Max"))
+//                .previewDisplayName("iPhone XS Max")
+//            IndicatorTabView(currentTab: selectedTabBinding, tabItems: tabItems)
+//                .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+//                .previewDisplayName("iPhone SE")
+//            IndicatorTabView(currentTab: selectedTabBinding, tabItems: tabItems)
+//                .previewDevice(PreviewDevice(rawValue: "iPad Air 2"))
+//                .previewDisplayName("iPad Air 2")
+//        }
+//    }
+//}
+//
