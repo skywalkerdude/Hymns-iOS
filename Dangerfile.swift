bdb416b8ee170b11d72859e4e27e769b5e888b09
Luke Lu <skywalkerdude@gmail.com>
2020-05-11 23:32:13 -0700

    syntax

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -41,7 +41,6 @@ func findXcresultFilePath() -> String {
     return testFolder + lastModifiedXcresult
 }
 
-let xcresultFilePath = try findXcresultFilePath()
+let xcresultFilePath = findXcresultFilePath()
 print(xcresultFilePath)
 //    Coverage.xcodeBuildCoverage(.xcresultBundle(xcresultFilePath), minimumCoverage: 50, excludedTargets: ["HymnTests.xctest", "HymnsUITests.xctest"])
-}
