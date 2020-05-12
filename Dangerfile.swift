1045f3820c79f6f1f61dd60192dc5b69cd65edf9
Luke Lu <skywalkerdude@gmail.com>
2020-05-11 20:01:49 -0700

    Upload xcresult data for Coverage report

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,25 +1,6 @@
 // swiftlint:disable all
 import Danger
-import DangerXCodeSummary // package: https://github.com/f-meloni/danger-swift-xcodesummary.git
+import DangerSwiftCoverage // package: https://github.com/f-meloni/danger-swift-coverage.git
 
-let danger = Danger()
-
-// Ensure no copyright header
-let changedFiles = (danger.git.modifiedFiles + danger.git.createdFiles).filter {
-    $0 != "Dangerfile.swift"
-}
-let swiftFilesWithCopyright = changedFiles.filter {
-    return $0.fileType == .swift && danger.utils.readFile($0).contains("//  Created by")
-}
-if swiftFilesWithCopyright.count > 0 {
-    let files = swiftFilesWithCopyright.joined(separator: ", ")
-    fail("Please remove the copyright header in: \(files)")
-}
-
-// Encourage smaller PRs
-var bigPRThreshold = 1000;
-if (danger.github.pullRequest.additions! + danger.github.pullRequest.deletions! > bigPRThreshold) {
-    warn("> Pull Request size seems relatively large. If this Pull Request contains multiple changes, please split each into separate PR will helps faster, easier review.");
-}
-
-//Coverage.xcodeBuildCoverage(.derivedDataFolder("Build"), minimumCoverage: 50, excludedTargets: ["DangerSwiftCoverageTests.xctest"])
+// Generate coverage data
+Coverage.xcodeBuildCoverage(.xcresultBundle("Hymns-*/Logs/Test/*.xcresult"), minimumCoverage: 50, excludedTargets: ["HymnTests.xctest", "HymnsUITests.xctest"])
