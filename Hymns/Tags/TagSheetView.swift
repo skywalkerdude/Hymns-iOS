8a558bbb7434a860b77518fe0322f85811d9e513
finben <benfindeisen@gmail.com>
2020-06-01 22:02:25 -0500

    Implement little tag buttons when you tag a hymn

--- Hymns/Tags/TagSheetView.swift
+++ Hymns/Tags/TagSheetView.swift
@@ -9,21 +9,47 @@ struct TagSheetView: View {
     @State private var tagNames = ""
     var title: String
     var hymnIdentifier: HymnIdentifier
-    
+    var fetchRequest: FetchRequest<TaggedHymn>
+
+    //@FetchRequest<TaggedHymn>(entity: TaggedHymn.entity(), sortDescriptors: []) var taggedHymn: FetchedResults<TaggedHymn>
+
+    init(title: String, hymnIdentifier: HymnIdentifier) {
+        fetchRequest = FetchRequest<TaggedHymn>(entity: TaggedHymn.entity(), sortDescriptors: [], predicate: NSPredicate(format: "songTitle BEGINSWITH %@", title))
+        self.hymnIdentifier = hymnIdentifier
+        self.title = title
+    }
+
     var body: some View {
         Form {
             Section {
                 Text("Label your tag")
                 TextField("Add tags", text: self.$tagNames)
             }
+            Button("ADD") {
+                let newTag = TaggedHymn(context: self.moc)
+                newTag.tagName = self.tagNames
+                newTag.songTitle = self.title
+                newTag.hymnIdentifier = FavoriteEntity.createPrimaryKey(hymnIdentifier: self.hymnIdentifier)
+                try? self.moc.save()
+                self.presentationMode.wrappedValue.dismiss()
+            }
             Section {
-                Button("ADD") {
-                    let newTag = TaggedHymn(context: self.moc)
-                    newTag.tagName = self.tagNames
-                    newTag.songTitle = self.title
-                    newTag.hymnIdentifier = FavoriteEntity.createPrimaryKey(hymnIdentifier: self.hymnIdentifier)
-                    try? self.moc.save()
-                    self.presentationMode.wrappedValue.dismiss()
+                HStack {
+                    ForEach(self.fetchRequest.wrappedValue, id: \.self) { tag in
+                        Button(action: {
+                            //TODO: Tags should be deletable here.
+                        }, label: {
+                            HStack {
+                                Text("\(tag.tagName ?? "" )")
+                                Image(systemName: "xmark.circle")
+                            }
+                        })
+                        .padding()
+                        .foregroundColor(.primary)
+                        .background(Color.orange)
+                        .cornerRadius(.infinity)
+                        .lineLimit(1)
+                    }
                 }
             }
         }
