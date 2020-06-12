0d2a20aae865843adf50d419d5729be0e1b48862
finben <benfindeisen@gmail.com>
2020-06-11 23:00:00 -0500

    Everything for the about us page

---
+++ Hymns/Settings/AboutUsButtonView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct AbousUsButtonView: View {
+    @State var isShowingInfo = false
+
+    var body: some View {
+        Button(action: {
+            self.isShowingInfo.toggle()
+        }, label: {
+            Text("About Us").font(.callout)
+        }).padding().foregroundColor(.primary)
+            .sheet(isPresented: $isShowingInfo) {
+                AbousUsInfoView()
+        }
+    }
+}
+
+struct AbousUsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AbousUsButtonView()
+    }
+}
