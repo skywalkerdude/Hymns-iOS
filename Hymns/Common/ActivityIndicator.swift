07a55cfc6b2cce320e6278c98ffd95ceeab862a5
Luke Lu <skywalkerdude@gmail.com>
2020-06-14 12:13:57 -0700

    swiftlint autocorrect

--- Hymns/Common/ActivityIndicator.swift
+++ Hymns/Common/ActivityIndicator.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
 struct ActivityIndicator: UIViewRepresentable {
-    
+
     func makeUIView(context: Context) -> UIActivityIndicatorView {
         return UIActivityIndicatorView()
     }
-    
+
     func updateUIView(_ uiView: UIActivityIndicatorView,
                       context: Context) {
         uiView.startAnimating()
