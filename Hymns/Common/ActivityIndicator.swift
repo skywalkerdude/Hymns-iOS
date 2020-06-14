3b0b1409ab4d7328d76b5dabde70ee9466b54256
Luke Lu <skywalkerdude@gmail.com>
2020-06-14 11:04:16 -0700

    Fix up bottom bar label coloring and add a buffering state with a loading indicator

--- Hymns/Common/ActivityIndicator.swift
+++ Hymns/Common/ActivityIndicator.swift
@@ -1,13 +1,13 @@
 import SwiftUI
 
 struct ActivityIndicator: UIViewRepresentable {
-
+    
     func makeUIView(context: Context) -> UIActivityIndicatorView {
         return UIActivityIndicatorView()
     }
-
+    
     func updateUIView(_ uiView: UIActivityIndicatorView,
                       context: Context) {
-            uiView.startAnimating()
+        uiView.startAnimating()
     }
 }
