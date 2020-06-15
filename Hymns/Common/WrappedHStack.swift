4311a7e70ce88ac1597fc4a45029e4709eb59859
finben <benfindeisen@gmail.com>
2020-06-14 22:48:02 -0500

    hehe it works. Passing a whole view like a view model interesting

--- Hymns/Common/WrappedHStack.swift
+++ Hymns/Common/WrappedHStack.swift
@@ -2,46 +2,60 @@ import SwiftUI
 
 // https://stackoverflow.com/a/58876712/1907538
 struct WrappedHStack<Item: Hashable, Content: View>: View {
-
+    
+    var view: TagSheetView
     @Binding var items: [Item]
     let viewBuilder: (Item) -> Content
-
+    
     var body: some View {
         GeometryReader { geometry in
             self.generateContent(in: geometry)
         }
     }
-
+    
     private func generateContent(in geometry: GeometryProxy) -> some View {
         guard let lastItem = self.items.last else {
             return EmptyView().eraseToAnyView()
         }
-
+        
         var width = CGFloat.zero
         var height = CGFloat.zero
-
-        return ZStack(alignment: .topLeading) {
-            ForEach(self.items, id: \.self) { item in
-                self.viewBuilder(item).alignmentGuide(.leading, computeValue: { dimension in
-                    if abs(width - dimension.width) > geometry.size.width {
-                        width = 0
-                        height -= dimension.height
-                    }
-                    let result = width
-                    if item == lastItem {
-                        width = 0 //last item
-                    } else {
-                        width -= dimension.width
-                    }
-                    return result
-                }).alignmentGuide(.top, computeValue: { _ in
-                    let result = height
-                    if item == lastItem {
-                        height = 0 // last item
-                    }
-                    return result
-                })
+        
+        return VStack {
+            ZStack(alignment: .topLeading) {
+                ForEach(self.items, id: \.self) { item in
+                    self.viewBuilder(item).alignmentGuide(.leading, computeValue: { dimension in
+                        if abs(width - dimension.width) > geometry.size.width {
+                            width = 0
+                            height -= dimension.height
+                        }
+                        let result = width
+                        if item == lastItem {
+                            width = 0 //last item
+                        } else {
+                            width -= dimension.width
+                        }
+                        return result
+                    }).alignmentGuide(.top, computeValue: { _ in
+                        let result = height
+                        if item == lastItem {
+                            height = 0 // last item
+                        }
+                        return result
+                    })
+                }
             }
+            HStack {
+                Spacer()
+                Button(action: {
+                    self.view.sheet.wrappedValue = nil
+                }, label: {
+                    Text("Cancel").foregroundColor(.primary).fontWeight(.light)
+                })
+                Button("Add") {
+                    self.view.viewModel.addTag(tagTitle: self.view.tagName, tagColor: self.view.tagColor)
+                }.padding(.horizontal).disabled(self.view.tagName.isEmpty)
+            }.padding(.top)
         }.eraseToAnyView()
     }
 }
@@ -49,11 +63,11 @@ struct WrappedHStack<Item: Hashable, Content: View>: View {
 struct WrappedHStack_Previews: PreviewProvider {
     static var previews: some View {
         let empty =
-            WrappedHStack(items: Binding.constant([String]())) { item in
+            WrappedHStack(view: TagSheetView(viewModel: TagSheetViewModel(hymnToDisplay: PreviewHymnIdentifiers.cupOfChrist), sheet: Binding.constant(.tags)), items: Binding.constant(["aww"])) { item in
                 Text(item)
         }
         let platforms =
-            WrappedHStack(items: Binding.constant(["Nintendo", "XBox", "PlayStation", "Playstation 2", "Playstaition 3", "Stadia", "Oculus"])) { platform in
+            WrappedHStack(view: TagSheetView(viewModel: TagSheetViewModel(hymnToDisplay: PreviewHymnIdentifiers.cupOfChrist), sheet: Binding.constant(.tags)), items: Binding.constant(["Nintendo", "XBox", "PlayStation", "Playstation 2", "Playstaition 3", "Stadia", "Oculus"])) { platform in
                 Button(action: {}, label: {
                     HStack {
                         Text(platform).font(.body).fontWeight(.bold)
--- Hymns/Tags/TagSheetView.swift
+++ Hymns/Tags/TagSheetView.swift
@@ -3,10 +3,10 @@ import Resolver
 
 struct TagSheetView: View {
 
-    @ObservedObject private var viewModel: TagSheetViewModel
+    @ObservedObject var viewModel: TagSheetViewModel
     @Environment(\.presentationMode) var presentationMode
-    @State private var tagName = ""
-    @State private var tagColor = TagColor.none
+    @State var tagName = ""
+    @State var tagColor = TagColor.none
     var sheet: Binding<DisplayHymnSheet?>
 
     init(viewModel: TagSheetViewModel, sheet: Binding<DisplayHymnSheet?>) {
@@ -20,21 +20,24 @@ struct TagSheetView: View {
             TextField("Label it however you like", text: self.$tagName)
             Divider()
             ColorSelectorView(tagColor: self.$tagColor).padding(.vertical)
-            HStack {
-                Spacer()
-                Button(action: {
-                    self.sheet.wrappedValue = nil
-                }, label: {
-                    Text("Cancel").foregroundColor(.primary).fontWeight(.light)
-                })
-                Button("Add") {
-                    self.viewModel.addTag(tagTitle: self.tagName, tagColor: self.tagColor)
-                }.padding(.horizontal).disabled(tagName.isEmpty)
-            }.padding(.top)
+            if self.viewModel.tags.isEmpty {
+
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.sheet.wrappedValue = nil
+                    }, label: {
+                        Text("Cancel").foregroundColor(.primary).fontWeight(.light)
+                    })
+                    Button("Add") {
+                        self.viewModel.addTag(tagTitle: self.tagName, tagColor: self.tagColor)
+                    }.padding(.horizontal).disabled(tagName.isEmpty)
+                }.padding(.top)
+            }
             if !self.viewModel.tags.isEmpty {
                 Section {
                     Text("Tags").font(.body).fontWeight(.bold)
-                    WrappedHStack(items: self.$viewModel.tags) { tag in
+                    WrappedHStack(view: self, items: self.$viewModel.tags) { tag in
                         Button(action: {
                             self.viewModel.deleteTag(tagTitle: tag.title, tagColor: tag.color)
                         }, label: {
@@ -43,7 +46,7 @@ struct TagSheetView: View {
                                 Image(systemName: "xmark.circle")
                             }
                             .tagPill(backgroundColor: tag.color.background, foregroundColor: tag.color.foreground)
-                            }).padding(5)
+                        }).padding(5)
                     }
                 }
             }
@@ -67,11 +70,11 @@ struct TagSheetView_Previews: PreviewProvider {
 
         let manyTagsViewModel = TagSheetViewModel(hymnToDisplay: PreviewHymnIdentifiers.cupOfChrist)
         manyTagsViewModel.tags = [UiTag(title: "Long tag name", color: .none),
-                          UiTag(title: "Tag 1", color: .green),
-                          UiTag(title: "Tag 1", color: .red),
-                          UiTag(title: "Tag 1", color: .yellow),
-                          UiTag(title: "Tag 2", color: .blue),
-                          UiTag(title: "Tag 3", color: .blue)]
+                                  UiTag(title: "Tag 1", color: .green),
+                                  UiTag(title: "Tag 1", color: .red),
+                                  UiTag(title: "Tag 1", color: .yellow),
+                                  UiTag(title: "Tag 2", color: .blue),
+                                  UiTag(title: "Tag 3", color: .blue)]
         let manyTags = TagSheetView(viewModel: manyTagsViewModel, sheet: Binding.constant(.tags))
         return Group {
             noTags
