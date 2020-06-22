83451c8d77d9c2a136a70c3bccbe0b90e239f9e1
Luke <skywalkerdude@gmail.com>
2020-06-22 11:59:55 -0700

    Add snapshot test for browse categories. Since we cannot manipulate the isExpanded state at the view level, we cannot test expanding/collapsing each category here, and will have to cover that with UI tests. (#338)

---
+++ HymnsSnapshotTests/BrowseCategoriesSnapshots.swift
@@ -0,0 +1,39 @@
+import SnapshotTesting
+import SwiftUI
+import XCTest
+@testable import Hymns
+
+// https://troz.net/post/2020/swiftui_snapshots/
+class BrowseCategoriesSnapshots: XCTestCase {
+
+    var viewModel: BrowseCategoriesViewModel!
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    func test_categories_error() {
+        viewModel = BrowseCategoriesViewModel(hymnType: .classic)
+        viewModel.categories = nil
+        assertSnapshot(matching: BrowseCategoriesView(viewModel: viewModel), as: .image())
+    }
+
+    func test_categories_loading() {
+        viewModel = BrowseCategoriesViewModel(hymnType: .classic)
+        assertSnapshot(matching: BrowseCategoriesView(viewModel: viewModel), as: .image())
+    }
+
+    func test_categories_results() {
+        viewModel = BrowseCategoriesViewModel(hymnType: nil)
+        viewModel.categories
+            = [CategoryViewModel(category: "Category 1", subcategories: [SubcategoryViewModel(subcategory: "Subcategory 1", count: 15),
+                                                                         SubcategoryViewModel(subcategory: "Subcategory 2", count: 2)]),
+               CategoryViewModel(category: "Category 2", subcategories: [SubcategoryViewModel(subcategory: "Subcategory 2", count: 12),
+                                                                         SubcategoryViewModel(subcategory: "Subcategory 3", count: 1)])]
+        assertSnapshot(matching: BrowseCategoriesView(viewModel: viewModel), as: .image())
+    }
+
+    func test_browseView() {
+        assertSnapshot(matching: BrowseView(), as: .image())
+    }
+}
