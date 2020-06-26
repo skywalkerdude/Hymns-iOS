cf9f97972ce221f075076218821c46b7537f66c5
Luke Lu <skywalkerdude@gmail.com>
2020-06-26 00:21:43 -0700

    Add scenario test for tapping the different tabs on the Home screen

--- HymnsUITests/Actions/HomeViewCan.swift
+++ HymnsUITests/Actions/HomeViewCan.swift
@@ -21,4 +21,44 @@ public class HomeViewCan: BaseViewCan {
         app.buttons[result].tap()
         return DisplayHymnViewCan(app, testCase: testCase)
     }
+
+    public func tapHome() -> HomeViewCan {
+        app.tabBars.children(matching: .button).element(boundBy: 0).tap()
+        return self
+    }
+
+    public func verifyHomeTab() -> HomeViewCan {
+        XCTAssertTrue(app.staticTexts["Look up any hymn"].exists)
+        return self
+    }
+
+    public func tapBrowse() -> HomeViewCan {
+        app.tabBars.children(matching: .button).element(boundBy: 1).tap()
+        return self
+    }
+
+    public func verifyBrowseTab() -> HomeViewCan {
+        XCTAssertTrue(app.staticTexts["Browse"].exists)
+        return self
+    }
+
+    public func tapFavorites() -> HomeViewCan {
+        app.tabBars.children(matching: .button).element(boundBy: 2).tap()
+        return self
+    }
+
+    public func verifyFavoritesTab() -> HomeViewCan {
+        XCTAssertTrue(app.staticTexts["Favorites"].exists)
+        return self
+    }
+
+    public func tapSettings() -> HomeViewCan {
+        app.tabBars.children(matching: .button).element(boundBy: 3).tap()
+        return self
+    }
+
+    public func verifySettingsTab() -> HomeViewCan {
+        XCTAssertTrue(app.staticTexts["Settings"].exists)
+        return self
+    }
 }
