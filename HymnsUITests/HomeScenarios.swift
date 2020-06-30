d0ab38ffbd7c852f7d16b5389f790a315605b1b7
Luke Lu <skywalkerdude@gmail.com>
2020-06-30 00:53:10 -0700

    commit

--- HymnsUITests/HomeScenarios.swift
+++ HymnsUITests/HomeScenarios.swift
@@ -36,6 +36,32 @@ class HomeScenarios: BaseTestCase {
             .verifyCancelNotExists()
     }
 
+    func test_goToSongFromRecentSongs() {
+        _ = HomeViewCan(app, testCase: self)
+            .waitForButtons("classic1151", "classic40", "classic2", "classic3")
+            .tapResult("classic1151")
+            .waitForStaticTexts("verse 1 line 1")
+    }
+
+    func test_goToSongFromNumber() {
+        _ = HomeViewCan(app, testCase: self)
+            .activateSearch()
+            .waitForButtons("Cancel")
+            .typeSearchText("1151")
+            .waitForButtons("Hymn 1151")
+            .tapResult("Hymn 1151")
+            .waitForStaticTexts("verse 1 line 1")
+    }
+
+    func test_goToSongFromSearchResults() {
+        _ = HomeViewCan(app, testCase: self)
+            .activateSearch()
+            .typeSearchText("search param")
+            .waitForButtons("Click me!")
+            .tapResult("Click me!")
+            .waitForStaticTexts("verse 1 line 1")
+    }
+
     func test_launchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
