9cc6e37411cb3f620e31c6849c2a5e9346f26ae8
Luke Lu <skywalkerdude@gmail.com>
2020-07-03 22:50:32 -0700

    Add UI test for the share sheet and add font action sheet

--- HymnsUITests/Actions/DisplayHymnViewCan.swift
+++ HymnsUITests/Actions/DisplayHymnViewCan.swift
@@ -6,4 +6,19 @@ public class DisplayHymnViewCan: BaseViewCan {
     override init(_ app: XCUIApplication, testCase: XCTestCase) {
         super.init(app, testCase: testCase)
     }
+
+    public func openShareSheet() -> DisplayHymnViewCan {
+        app.buttons["square.and.arrow.up"].tap()
+        return self
+    }
+
+    public func openFontPicker() -> DisplayHymnViewCan {
+        app.buttons["textformat.size"].tap()
+        return self
+    }
+
+    public func openTagSheet() -> DisplayHymnViewCan {
+        app.buttons["tag"].tap()
+        return self
+    }
 }
--- HymnsUITests/HomeScenarios.swift
+++ HymnsUITests/HomeScenarios.swift
@@ -7,6 +7,32 @@ class HomeScenarios: BaseTestCase {
         app.launch()
     }
 
+    func test_goToSongFromRecentSongs() {
+        _ = HomeViewCan(app, testCase: self)
+            .waitForButtons("classic1151", "classic40", "classic2", "classic3")
+            .tapResult("classic1151")
+            .waitForStaticTexts("verse 1 line 1")
+    }
+
+    func test_goToSongFromNumber() {
+        _ = HomeViewCan(app, testCase: self)
+            .activateSearch()
+            .waitForButtons("Cancel")
+            .typeSearchText("1151")
+            .waitForButtons("Hymn 1151")
+            .tapResult("Hymn 1151")
+            .waitForStaticTexts("verse 1 line 1")
+    }
+
+    func test_goToSongFromSearchResults() {
+        _ = HomeViewCan(app, testCase: self)
+            .activateSearch()
+            .typeSearchText("search param")
+            .waitForButtons("Click me!")
+            .tapResult("Click me!")
+            .waitForStaticTexts("verse 1 line 1")
+    }
+
     func test_switchBetweenTabs() {
         _ = HomeViewCan(app, testCase: self)
             .verifyHomeTab()
