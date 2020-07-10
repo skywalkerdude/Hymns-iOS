4e6e8715358a4f2a9baf7f6c4c4655b59b3e1a35
Luke Lu <skywalkerdude@gmail.com>
2020-07-10 00:13:44 -0700

    Fix https://github.com/HymnalDreamTeam/Hymns-iOS/issues/377 and add snapshot test

--- Hymns/Settings/AboutUsDialogView.swift
+++ Hymns/Settings/AboutUsDialogView.swift
@@ -20,19 +20,19 @@ struct AboutUsDialogView: View {
                     Spacer()
                 }.padding().padding(.top).foregroundColor(.primary)
                 Text("Hello There 👋").font(.title).fontWeight(.bold).padding()
-                Text("We're the team behind this hymnal app. We love Jesus, and we created this app as a free resource to help other believers access the thousands of hymns available on the internet. We also built in support for the hymns indexed by the Living Stream Ministry hymnal.").padding()
+                Text("We're the team behind this hymnal app. We love Jesus, and we created this app as a free resource to help other believers access the thousands of hymns available on the internet. We also built in support for the hymns indexed by the Living Stream Ministry hymnal.").font(.callout).padding()
                 Text("Let the word of Christ dwell in you richly in all wisdom, teaching and admonishing one another with psalms and hymns and spiritual songs, singing with grace in your hearts to God.")
-                    .padding().padding(.horizontal)
+                    .font(.body).fontWeight(.light).padding().padding(.horizontal)
                 HStack {
                     Spacer()
-                    Text("- Col. 3:16").font(.callout).fontWeight(.bold).padding(.trailing)
+                    Text("- Col. 3:16").font(.body).fontWeight(.bold).padding(.trailing)
                 }
                 // Cant get this dumb thing in line. It won't concatenate the text since it has a tapGesture.
                 // Right now the whole sentence will link you.
                 // https://stackoverflow.com/questions/59359730/is-it-possible-to-add-an-in-line-button-within-a-text
                 Group {
                     Text("For a free study Bible tap ") + Text("here.").fontWeight(.bold).underline()
-                }.padding().onTapGesture {
+                }.font(.callout).padding().onTapGesture {
                     UIApplication.shared.open(URL(string: "https://biblesforamerica.org/")!)
                     self.analytics.logBFALinkClicked()
                 }
