210f8649e38399cb158fff35358c4899763b4382
Luke <skywalkerdude@gmail.com>
2020-07-24 13:41:35 -0700

    Add preloading of the audio as soon as the view appears. Also fix the audio-finished-playing listener to respect the repeat boolean (#395)

--- HymnsTests/Audio Player/AudioPlayerViewModelSpec.swift
+++ HymnsTests/Audio Player/AudioPlayerViewModelSpec.swift
@@ -26,31 +26,7 @@ class AudioPlayerViewModelSpec: QuickSpec {
                     expect(target.shouldRepeat).to(beFalse())
                 }
             }
-            context("unsuccessful music fetch") {
-                beforeEach {
-                    let path = Bundle(for: AudioPlayerViewModelSpec.self).path(forResource: "e0767_i", ofType: "mp3")!
-                    // swiftlint:disable:next force_try
-                    let data = try! Data(contentsOf: URL(fileURLWithPath: path))
-                    given(service.getData(url)) ~> { _ in
-                        Just(data)
-                            .tryMap({ _ -> Data in
-                                throw URLError(.badServerResponse)
-                            }).mapError({ _ -> ErrorType in
-                                .data(description: "forced data error")
-                            }).eraseToAnyPublisher()
-                    }
-
-                    target.play()
-                    testQueue.sync {}
-                    testQueue.sync {}
-                    testQueue.sync {}
-                }
-                it("should set playback state to stopped") {
-                    verify(service.getData(url)).wasCalled(1)
-                    expect(target.playbackState).to(equal(.stopped))
-                }
-            }
-            context("successful music fetch") {
+            context("successfully preload music") {
                 beforeEach {
                     let path = Bundle(for: AudioPlayerViewModelSpec.self).path(forResource: "e0767_i", ofType: "mp3")!
                     // swiftlint:disable:next force_try
@@ -61,50 +37,171 @@ class AudioPlayerViewModelSpec: QuickSpec {
                         }).eraseToAnyPublisher()
                     }
 
-                    target.play()
+                    target.load()
                     testQueue.sync {}
                     testQueue.sync {}
                     testQueue.sync {}
-                }
-                it("should set playback state to playing") {
                     verify(service.getData(url)).wasCalled(1)
-                    expect(target.playbackState).to(equal(.playing))
                 }
-                describe("pause") {
-                    beforeEach {
-                        target.pause()
-                    }
-                    it("should set playback state to stopped") {
-                        expect(target.playbackState).to(equal(.stopped))
-                    }
-                    describe("play again") {
-                        beforeEach {
-                            reset(service)
-                            target.play()
-                        }
-                        it("should set playback state to playing without calling the service") {
-                            expect(target.playbackState).to(equal(.playing))
-                        }
-                    }
+                it("should set the delegate to itself") {
+                    expect(target.player!.delegate).to(be(target))
                 }
-                describe("reset") {
+                describe("playing the music") {
                     beforeEach {
-                        target.reset()
+                        clearInvocations(on: service)
+                        target.play()
                     }
-                    it("should set playback state to stopped") {
-                        expect(target.playbackState).to(equal(.stopped))
+                    it("should not call the service") {
+                        verify(service.getData(any())).wasNeverCalled()
                     }
-                    describe("play again") {
+                    it("should play the music") {
+                        expect(target.playbackState).to(equal(.playing))
+                        expect(target.player!.isPlaying).to(beTrue())
+                    }
+                    describe("pause") {
                         beforeEach {
-                            clearInvocations(on: service)
-                            target.play()
+                            target.pause()
+                        }
+                        it("should pause the playback") {
+                            expect(target.playbackState).to(equal(.stopped))
+                            expect(target.player!.isPlaying).to(beFalse())
+                        }
+                        describe("play again") {
+                            beforeEach {
+                                target.play()
+                            }
+                            it("should not call the service") {
+                                verify(service.getData(any())).wasNeverCalled()
+                            }
+                            it("should play the music") {
+                                expect(target.playbackState).to(equal(.playing))
+                                expect(target.player!.isPlaying).to(beTrue())
+                            }
+                        }
+                    }
+                    describe("reset") {
+                        beforeEach {
+                            target.reset()
                             testQueue.sync {}
                             testQueue.sync {}
                             testQueue.sync {}
                         }
-                        it("should call the service again and set the playback to playing") {
+                        it("should set repeat to false") {
+                            expect(target.shouldRepeat).to(beFalse())
+                        }
+                        it("should stop the playback") {
+                            expect(target.playbackState).to(equal(.stopped))
+                            expect(target.player!.isPlaying).to(beFalse())
+                        }
+                        it("should reload the song") {
                             verify(service.getData(url)).wasCalled(1)
-                            expect(target.playbackState).to(equal(.playing))
+                        }
+                        describe("play again") {
+                            beforeEach {
+                                reset(service)
+                                target.play()
+                            }
+                            it("should not call the service") {
+                                verify(service.getData(any())).wasNeverCalled()
+                            }
+                            it("should play the music") {
+                                expect(target.playbackState).to(equal(.playing))
+                                expect(target.player!.isPlaying).to(beTrue())
+                            }
+                        }
+                    }
+                }
+            }
+            describe("do not preload music") {
+                context("unsuccessful music fetch") {
+                    beforeEach {
+                        let path = Bundle(for: AudioPlayerViewModelSpec.self).path(forResource: "e0767_i", ofType: "mp3")!
+                        // swiftlint:disable:next force_try
+                        let data = try! Data(contentsOf: URL(fileURLWithPath: path))
+                        given(service.getData(url)) ~> { _ in
+                            Just(data)
+                                .tryMap({ _ -> Data in
+                                    throw URLError(.badServerResponse)
+                                }).mapError({ _ -> ErrorType in
+                                    .data(description: "forced data error")
+                                }).eraseToAnyPublisher()
+                        }
+
+                        target.play()
+                        testQueue.sync {}
+                        testQueue.sync {}
+                        testQueue.sync {}
+                        verify(service.getData(url)).wasCalled(1)
+                    }
+                    it("should stolp the playback") {
+                        expect(target.playbackState).to(equal(.stopped))
+                        expect(target.player).to(beNil())
+                    }
+                }
+                context("successful music fetch") {
+                    beforeEach {
+                        let path = Bundle(for: AudioPlayerViewModelSpec.self).path(forResource: "e0767_i", ofType: "mp3")!
+                        // swiftlint:disable:next force_try
+                        let data = try! Data(contentsOf: URL(fileURLWithPath: path))
+                        given(service.getData(url)) ~> { _ in
+                            Just(data).mapError({ _ -> ErrorType in
+                                .data(description: "This will never get called")
+                            }).eraseToAnyPublisher()
+                        }
+
+                        target.play()
+                        testQueue.sync {}
+                        testQueue.sync {}
+                        testQueue.sync {}
+                        verify(service.getData(url)).wasCalled(1)
+                    }
+                    it("should play the music") {
+                        expect(target.playbackState).to(equal(.playing))
+                        expect(target.player!.isPlaying).to(beTrue())
+                    }
+                    describe("pause") {
+                        beforeEach {
+                            target.pause()
+                        }
+                        it("should pause the playback") {
+                            expect(target.playbackState).to(equal(.stopped))
+                            expect(target.player!.isPlaying).to(beFalse())
+                        }
+                        describe("play again") {
+                            beforeEach {
+                                reset(service)
+                                target.play()
+                            }
+                            it("should not call the service") {
+                                verify(service.getData(any())).wasNeverCalled()
+                            }
+                            it("should play the music") {
+                                expect(target.playbackState).to(equal(.playing))
+                                expect(target.player!.isPlaying).to(beTrue())
+                            }
+                        }
+                    }
+                    describe("playback finished") {
+                        context("should repeat") {
+                            beforeEach {
+                                target.shouldRepeat = true
+                                target.audioPlayerDidFinishPlaying(target.player!, successfully: true)
+                            }
+                            it("should play from the beginning") {
+                                expect(target.player!.currentTime).to(equal(TimeInterval.zero))
+                                expect(target.playbackState).to(equal(.playing))
+                            }
+                        }
+                        context("should not repeat") {
+                            beforeEach {
+                                target.shouldRepeat = false
+                                target.audioPlayerDidFinishPlaying(target.player!, successfully: true)
+                            }
+                            it("should stop playing") {
+                                expect(target.player!.currentTime).to(equal(TimeInterval.zero))
+                                expect(target.playbackState).to(equal(.stopped))
+                                expect(target.player!.isPlaying).to(beFalse())
+                            }
                         }
                     }
                 }
