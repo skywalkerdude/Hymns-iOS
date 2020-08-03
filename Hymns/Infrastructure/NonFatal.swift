4b616ef818716e623357025bd8e6b76c7aa9fb73
Luke Lu <skywalkerdude@gmail.com>
2020-08-02 17:17:05 -0700

    Change non-fatal error description to localized description to hopefully be more informative in the Firebase Crashlytics dashboard

--- Hymns/Infrastructure/NonFatal.swift
+++ Hymns/Infrastructure/NonFatal.swift
@@ -5,5 +5,5 @@ import Foundation
  * Non-fatal error that shouldn't crash the app, but also shouldn't ever happen in the wild.
  */
 struct NonFatal: Error, Equatable {
-    let errorDescription: String
+    let localizedDescription: String
 }
