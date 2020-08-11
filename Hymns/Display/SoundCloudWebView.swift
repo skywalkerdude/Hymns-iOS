ebcd6395b91b8c589dc1c94b325df0406b7906e8
Luke <skywalkerdude@gmail.com>
2020-08-10 20:43:43 -0700

    Clean up code in SoundCloudWebView that does nothing. If we want to listen to the loading state, we should follow https://medium.com/@mdyamin/swiftui-mastering-webview-5790e686833e or https://developer.apple.com/forums/thread/123139 (#442)

--- Hymns/Display/SoundCloudWebView.swift
+++ Hymns/Display/SoundCloudWebView.swift
@@ -3,10 +3,9 @@ import WebKit
 
 struct SoundCloudWebView: UIViewRepresentable {
     var url: URL
-    var loadStatusChanged: ((Bool, Error?) -> Void)?
 
     func makeCoordinator() -> SoundCloudWebView.Coordinator {
-        Coordinator(self)
+        Coordinator()
     }
 
     func makeUIView(context: Context) -> WKWebView {
@@ -19,22 +18,9 @@ struct SoundCloudWebView: UIViewRepresentable {
     func updateUIView(_ uiView: WKWebView, context: Context) {
     }
 
-    func onLoadStatusChanged(perform: ((Bool, Error?) -> Void)?) -> some View {
-        var copy = self
-        copy.loadStatusChanged = perform
-        return copy
-    }
-
     class Coordinator: NSObject, WKNavigationDelegate {
-        let parent: SoundCloudWebView
 
-        init(_ parent: SoundCloudWebView) {
-            self.parent = parent
-        }
-
-        func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-            parent.loadStatusChanged?(true, nil)
-        }
+        override init() {}
 
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
             // Remove the header banner so we don't have two SoundCloud logos on top of each other
@@ -46,11 +32,6 @@ struct SoundCloudWebView: UIViewRepresentable {
             // Make the top padding 0px because we are removing the upsell and the header
             webView.evaluateJavaScript("document.getElementById('content').style.paddingTop = '0px'",
                                        completionHandler: { (_, _) -> Void in })
-            parent.loadStatusChanged?(false, nil)
-        }
-
-        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-            parent.loadStatusChanged?(false, error)
         }
     }
 }
