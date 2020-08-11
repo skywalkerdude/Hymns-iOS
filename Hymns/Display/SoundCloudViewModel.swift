79c6d9c7f870b2b0d1ba2e1ee49779a1d2ba1cf1
finben <benfindeisen@gmail.com>
2020-08-10 20:05:12 -0500

    getting tooltip to work on time

--- Hymns/Display/SoundCloudViewModel.swift
+++ Hymns/Display/SoundCloudViewModel.swift
@@ -9,13 +9,19 @@ class SoundCloudViewModel: ObservableObject {
             userDefaultsManager.showSoundCloudMinimizeTooltip = newValue
         }
     }
+    @Published var activeTimer: Timer.TimerPublisher
 
     init(userDefaultsManager: UserDefaultsManager = Resolver.resolve()) {
         self.userDefaultsManager = userDefaultsManager
         self.showSoundCloudMinimizeTooltip = userDefaultsManager.showSoundCloudMinimizeTooltip
+        self.activeTimer = Timer.publish(every: 2, on: .main, in: .common)
+        if userDefaultsManager.showSoundCloudMinimizeTooltip {
+            self.activeTimer.connect()
+        }
     }
 
     func dismissToolTip() {
         showSoundCloudMinimizeTooltip = false
+        self.activeTimer.connect().cancel()
     }
 }
