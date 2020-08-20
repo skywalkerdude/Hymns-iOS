d1b8d590e2438757f366efd9809e4af4b892c614
Luke <skywalkerdude@gmail.com>
2020-08-20 15:32:07 -0700

    Add snapshot test for privacy policy view (#458)
    * Add snapshot test for privacy policy view
    
    * make privacy policy timeout 60 seconds

--- Hymns/Settings/PrivacyPolicySettingView.swift
+++ Hymns/Settings/PrivacyPolicySettingView.swift
@@ -9,20 +9,8 @@ struct PrivacyPolicySettingView: View {
         Button(action: {self.showPrivacyPolicy.toggle()}, label: {
             Text("Privacy policy").font(.callout)
         }).padding().foregroundColor(.primary)
-            .sheet(isPresented: self.$showPrivacyPolicy, content: { () -> AnyView in
-                guard let url = URL(string: "https://app.termly.io/document/privacy-policy/4b9dd46b-aca9-40ae-ac97-58b47e4b4cac") else {
-                    Crashlytics.crashlytics().log("Privacy policy url: 'https://app.termly.io/document/privacy-policy/4b9dd46b-aca9-40ae-ac97-58b47e4b4cac'")
-                    Crashlytics.crashlytics().record(error: NonFatal(localizedDescription: "Privacy policy url malformed"))
-                    return ErrorView().eraseToAnyView()
-                }
-                return VStack(alignment: .leading) {
-                    Button(action: {
-                        self.showPrivacyPolicy = false
-                    }, label: {
-                        Text("Close").padding([.top, .horizontal])
-                    })
-                    WebView(url: url)
-                }.eraseToAnyView()
+            .sheet(isPresented: self.$showPrivacyPolicy, content: {
+                PrivacyPolicyView(showPrivacyPolicy: self.$showPrivacyPolicy)
             })
     }
 }
